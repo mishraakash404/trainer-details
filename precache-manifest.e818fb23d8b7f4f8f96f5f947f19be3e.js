156bca4672680710507bf7a56d24b17271ac65d8
mishraakash404 <33085320+mishraakash404@users.noreply.github.com>
2021-02-25 17:46:25 +0530

    Updates

---
+++ precache-manifest.e818fb23d8b7f4f8f96f5f947f19be3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "616e5fee79fef9ed25c1",
+    "url": "/trainer-details/static/css/main.74edf7af.chunk.css"
+  },
+  {
+    "revision": "616e5fee79fef9ed25c1",
+    "url": "/trainer-details/static/js/main.db187342.chunk.js"
+  },
+  {
+    "revision": "c46abc59932bc245632c",
+    "url": "/trainer-details/static/js/runtime~main.2896a1ff.js"
+  },
+  {
+    "revision": "e9ea7175c5423036a89a",
+    "url": "/trainer-details/static/js/2.6a43b70e.chunk.js"
+  },
+  {
+    "revision": "8e8c09e8561212f1f2cb",
+    "url": "/trainer-details/static/js/3.8490d774.chunk.js"
+  },
+  {
+    "revision": "d69af5c5c3b1ab988b1e1e5b71f77826",
+    "url": "/trainer-details/static/media/pexels-mister-mister-3490348.d69af5c5.jpg"
+  },
+  {
+    "revision": "d697704d8faf05703daf38048dfa9880",
+    "url": "/trainer-details/static/media/pexels-ketut-subiyanto-4720236.d697704d.jpg"
+  },
+  {
+    "revision": "1306dab72707c7ff75e6170dcddeef46",
+    "url": "/trainer-details/static/media/pexels-victor-freitas-841130.1306dab7.jpg"
+  },
+  {
+    "revision": "d9e726babdebe91021a4454d819cee9e",
+    "url": "/trainer-details/index.html"
+  }
+];
